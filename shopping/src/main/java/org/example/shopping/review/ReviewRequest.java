564c66a7fdbf09208d707b52564dfed9b70fdd75
untilthe-end <letsgohangun@gmail.com>
2025-12-11 17:57:23 +0900

    review Controller Respository complete

--- shopping/src/main/java/org/example/shopping/review/ReviewRequest.java
+++ shopping/src/main/java/org/example/shopping/review/ReviewRequest.java
@@ -6,16 +6,16 @@ public class ReviewRequest {
 
     @Data
     public static class SaveDTO {
-        private String productId;
-        private String userId;
+//        private String productId;
+//        private String userId;
         private String content;
         private int rating;
         private String photoUrl;
 
         public Review toEntity() {
             return new Review(
-                    productId,
-                    userId,
+//                    productId,
+//                    userId,
                     rating,
                     content,
                     photoUrl
@@ -24,7 +24,20 @@ public class ReviewRequest {
     }
 
     @Data
-    public static class updateDTO {
+    public static class UpdateDTO {
+        private int rating;
+        private String content;
+        private String photoUrl;
 
+        // 검증 메서드
+        public void validate() {
+            if (rating == 0) {
+                throw new IllegalArgumentException("1~5 점을 선택하세요.");
+            }
+
+            if (content == null || content.trim().isEmpty()) {
+                throw new IllegalArgumentException("내용은 필수입니다.");
+            }
+        }
     }
 }
