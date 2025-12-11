3bfd89497d232e62c2fb6f62ff17d3d9f339a69e
Kitbomin <zzxxcc1433@naver.com>
2025-12-11 15:20:51 +0900

    annotaiton work

--- shopping/src/main/java/org/example/shopping/user/UserRole.java
+++ shopping/src/main/java/org/example/shopping/user/UserRole.java
@@ -11,13 +11,13 @@ public class UserRole {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_user_role_user"))
+//    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_user_role_user"))
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "role_name"
-            , referencedColumnName = "role_name"
-            , foreignKey = @ForeignKey(name = "fk_user_role_role"))
+//    @JoinColumn(name = "role_name"
+//            , referencedColumnName = "role_name"
+//            , foreignKey = @ForeignKey(name = "fk_user_role_role"))
     private Role role;
 
     public UserRole(User user, Role role) {
