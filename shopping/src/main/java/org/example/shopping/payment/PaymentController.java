b41042bea01c88d3c671dea65b6c23e0d99074f2
jihoonjeong56 <jeong283056@gmail.com>
2025-12-11 16:09:31 +0900

    payment approve ~ing

--- shopping/src/main/java/org/example/shopping/payment/PaymentController.java
+++ shopping/src/main/java/org/example/shopping/payment/PaymentController.java
@@ -3,6 +3,7 @@ package org.example.shopping.payment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -13,16 +14,33 @@ public class PaymentController {
 
     // 결제 생성
     @GetMapping("/payment/payment")
-    public String createPayment(){
+    public String createPaymentForm(){
         return "payment/payment-form";
     }
 
     @PostMapping("/payment/payment")
-    public String createProc(PaymentRequest.CreateDTO createDTO){
+    public String createPaymentProc(PaymentRequest.CreateDTO createDTO){
         Payment payment = createDTO.toEntity();
         repository.save(payment);
         return "redirect:/";
     }
 
-    // 결제 승인
+    // 결제 적용
+    @GetMapping("/payment/{id}/approve")
+    public String approvePaymentForm(@PathVariable Long id){
+        return "payment/approve-form";
+    }
+
+    @PostMapping("/payment/{id}/approve")
+    public String approvePaymentProc(@PathVariable Long id, PaymentRequest.ApproveDTO approveDTO){
+        try{
+            repository.approveById(id, approveDTO);
+
+        } catch (Exception e) {
+            throw new RuntimeException("결제 승인 실패");
+        }
+
+        return "redirect:/";
+    }
+
 }
