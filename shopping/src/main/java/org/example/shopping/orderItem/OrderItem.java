324a45e6323e1a721c8606a1b8342e9b70e89736
ehdgn <ehdgn6570@gmail.com>
2025-12-11 15:36:29 +0900

    order and orderItem entity complete

---
+++ shopping/src/main/java/org/example/shopping/orderItem/OrderItem.java
@@ -0,0 +1,40 @@
+package org.example.shopping.orderItem;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.shopping.order.Order;
+import org.example.shopping.utils.BaseTimeEntity;
+
+@Data
+@NoArgsConstructor
+@Table(name = "order_item_tb")
+@Entity
+public class OrderItem extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    Order order;
+
+    String product;
+
+    Integer quantity;
+
+    Long orderPrice;
+
+    Long totalPrice;
+
+    @Builder
+    public OrderItem(Long id, Order order, Long orderPrice, String product, Integer quantity, Long totalPrice) {
+        this.id = id;
+        this.order = order;
+        this.orderPrice = orderPrice;
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = orderPrice * quantity;
+    }
+}
