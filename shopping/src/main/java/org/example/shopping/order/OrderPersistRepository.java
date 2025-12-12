41a9075abfab6ce9e40856be7b23f0471fb39456
ehdgn <ehdgn6570@gmail.com>
2025-12-12 17:20:55 +0900

    order controller ~ing

--- shopping/src/main/java/org/example/shopping/order/OrderPersistRepository.java
+++ shopping/src/main/java/org/example/shopping/order/OrderPersistRepository.java
@@ -1,4 +1,9 @@
 package org.example.shopping.order;
 
-public class OrderPersistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderPersistRepository extends JpaRepository<Order, Long> {
+
 }
