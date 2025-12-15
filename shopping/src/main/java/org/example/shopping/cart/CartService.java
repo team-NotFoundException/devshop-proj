902dfb3a38e1e475a1bf086f04b2f131d42f1c83
ehdgn <ehdgn6570@gmail.com>
2025-12-15 17:27:27 +0900

    order-and-cart-service ~ing

--- shopping/src/main/java/org/example/shopping/cart/CartService.java
+++ shopping/src/main/java/org/example/shopping/cart/CartService.java
@@ -1,4 +1,75 @@
 package org.example.shopping.cart;
 
+import lombok.RequiredArgsConstructor;
+import org.example.shopping._core.errors.exception.Exception404;
+import org.example.shopping.cartItem.CartItem;
+import org.example.shopping.cartItem.CartItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CartService {
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    // 장바구니 아이템 목록
+    public Cart getCartItems(Long cartId) {
+
+        return cartRepository.findById(cartId)
+                .orElseThrow(() -> new Exception404("장바구니를 찾을 수 없습니다."));
+    }
+
+    // 아이템 추가
+    // 1. 트랜잭션 처리
+    // 2. DB 에서 카트 조회 (cartId) db를 덜 조회하는 방향
+    // 3. 조회된 cart에 아이템 추가
+    // +) 추가하는 아이템이 이미 장바구니에 있으면 [상품 id와 옵션(사이즈, 색깔 등) 확인]
+    // -> 오류 발생(이미 장바구니에 있는 아이템입니다.)
+    @Transactional
+    public Cart addCartItem(CartRequest.AddDTO addDTO, Long cartId) {
+
+        Cart cartEntity = cartRepository.findById(cartId)
+                .orElseThrow(() -> new Exception404("장바구니를 찾을 수 없습니다."));
+
+        cartEntity.addItem(addDTO.toEntity());
+
+        return cartEntity;
+    }
+
+    // 아이템 삭제
+    @Transactional
+    public void removeCartItem(Long cartId, Long cartItemId) {
+
+        CartItem cartItemEntity = cartItemRepository.findByCart_IdAndId(cartId, cartItemId)
+                .orElseThrow(() -> new Exception404("아이템을 찾을 수 없습니다."));
+
+        Cart cart = cartItemEntity.getCart();
+
+        cart.removeItem(cartItemEntity);
+    }
+
+    // 아이템 선택
+    // 1. 카트 조회
+    // 2. 카트 아이템 조회
+    // 3. 카트 아이템의 체크 상태 업데이트
+    @Transactional
+    public void checkItem(Long cartId ,Long cartItemId) {
+
+        CartItem cartItemEntity = cartItemRepository.findByCart_IdAndId(cartId, cartItemId)
+                .orElseThrow(() -> new Exception404("아이템을 찾을 수 없습니다."));
+
+        cartItemEntity.updateCheckItem();
+    }
+
+    // 아이템 개수/옵션 변경
+    @Transactional
+    public void updateOption(CartRequest.UpdateOptionDTO updateOptionDTO, Long cartId ,Long cartItemId) {
+
+        CartItem cartItemEntity = cartItemRepository.findByCart_IdAndId(cartId, cartItemId)
+                .orElseThrow(() -> new Exception404("아이템을 찾을 수 없습니다."));
+
+        cartItemEntity.updateItemOption(updateOptionDTO.getQuantity());
+    }
 }
--- shopping/src/main/java/org/example/shopping/order/OrderRequest.java
+++ shopping/src/main/java/org/example/shopping/order/OrderRequest.java
@@ -16,10 +16,18 @@ public class OrderRequest {
         private String paymentInfo;
         private Long totalPrice;
 
-
-
         public Order toEntity(String user) {
             return new Order(items, orderStatus, paymentInfo, totalPrice);
         }
+    }
+
+    public static class CartOrderDTO {
+
+        /*
+            1. 장바구니에서 아이템을 선택
+            2. 구매하기 버튼을 누르면 주문 생성 페이지로 넘어감
+            3. cartItem 에서 isChecked가 true인 아이템을 List<orderItem> items에 넣음
+            4. List<orderItem> items의 orderItem이 cartItem임
+         */
     }
 }
