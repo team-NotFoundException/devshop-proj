c2c1e68c845c78b77026dc1919d35c716b6c9f7e
ehdgn <ehdgn6570@gmail.com>
2025-12-22 17:36:34 +0900

    add cart db

--- shopping/src/main/java/org/example/shopping/order/OrderRepository.java
+++ shopping/src/main/java/org/example/shopping/order/OrderRepository.java
@@ -1,9 +1,16 @@
 package org.example.shopping.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @Query(value = "SELECT DISTINCT o FROM Order o JOIN FETCH o.payment ORDER BY o.createdAt DESC ",
+           countQuery = "SELECT COUNT(DISTINCT O) FROM Order o")
+    List<Order> findAllOrderByCreatedAt();
 }
