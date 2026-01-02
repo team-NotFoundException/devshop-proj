169d5ae8814d71205c6f94542469dddc5a176994
Kitbomin <zzxxcc1433@naver.com>
2026-01-02 17:51:54 +0900

    save

--- shopping/src/main/java/org/example/shopping/users/user/UserRepository.java
+++ shopping/src/main/java/org/example/shopping/users/user/UserRepository.java
@@ -1,6 +1,7 @@
 package org.example.shopping.users.user;
 
 import org.example.shopping.users.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +10,10 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // === 공통 === //
+
     Optional<User> findByUsername(String username);
 
-    Optional<User> findByUsernameAndPassword(String username, String password);
 
     // 로그인
 
@@ -21,17 +23,23 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 회원정보 수정
 
-    // 회원정보 조회
 
     // 로그인용 사용자 조회
 
     // 유저 전체 조회(권한 포함)
-//    @Query("SELECT u.username, u.nickname, u.email, u.role FROM User u LEFT JOIN UserRole r ON r.user_id = :u.user_id ORDER BY u.createdAt ASC")
-//    Optional<User> findAllByUsernameAndRole
 
     // 유저 권한 부여
 
     // 유저 권한 회수
 
+    // === OWNER 용 === //
 
+
+
+    // === 관리자용 === //
+    Optional<User> findAllByUsernameOrderByCreatedAt(String username, Pageable pageable);
+
+
+//    @Query("SELECT u.username, u.nickname, u.email, u.role FROM User u LEFT JOIN UserRole r ON r.user_id = :u.user_id ORDER BY u.createdAt ASC")
+    //    Optional<User> findAllByUsernameAndRole
 }
