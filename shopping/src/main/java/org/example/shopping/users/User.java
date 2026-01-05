ef562b7568c7465099d681b3314205eab407301a
Kitbomin <zzxxcc1433@naver.com>
2026-01-05 14:15:05 +0900

    feature-admin-mustache

--- shopping/src/main/java/org/example/shopping/users/User.java
+++ shopping/src/main/java/org/example/shopping/users/User.java
@@ -28,26 +28,18 @@ public class User extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 5, max = 255)
-    @NotBlank(message = "id는 필수 입력 항목입니다.")
     private String username;
 
-//    @Size(min = 6, max = 16)
-    @NotBlank(message = "pw는 필수 입력 항목입니다.")
     private String password;
 
-    @Size(min = 2, max = 6)
-    @NotBlank(message = "nickname은 필수 입력 항목입니다.")
     private String nickname;
 
-    @Email(message = "유효한 이메일 주소를 입력해주세요.")
-    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     private String email;
 
     @Embedded
     private Address address;
 
-    @NotBlank(message = "연락처는 필수 입력 항목입니다.")
+
     private String phoneNumber;
 
     @Enumerated(EnumType.STRING)
