ea63320fdff76b11a5f779c68edb4a3808d846b7
jihoonjeong56 <jeong283056@gmail.com>
2026-01-06 17:09:57 +0900

    delete user ~~~~

--- shopping/src/main/java/org/example/shopping/users/admin/AdminController.java
+++ shopping/src/main/java/org/example/shopping/users/admin/AdminController.java
@@ -8,12 +8,10 @@ import org.example.shopping.users.User;
 import org.example.shopping.users.dto.UserResponse;
 import org.example.shopping.users.enums.RoleType;
 import org.example.shopping.users.user.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,20 +37,12 @@ public class AdminController {
         return "user/admin/user-list";
     }
 
-    // 권한부여
-    @PostMapping("/admin/grant-role")
-    @ResponseBody
-    public String grantRole(@RequestParam Long userId, @RequestParam String role, HttpSession session){
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        try {
-            RoleType roleType = RoleType.valueOf(role.toUpperCase());
-            userService.grantRole(userId, roleType);
-            return "redirect:/user/admin/user-list";
-
-        }catch (Exception e){
-            throw new Exception500(e.getMessage());
-        }
-
+    // 회원삭제
+    @PostMapping("/admin/user/{userId}/delete")
+    public String deleteUser(HttpSession session,@PathVariable Long userId){
+        User sessionUser= (User) session.getAttribute("sessionUser");
+        userService.deleteUser(userId);
+        return "redirect:user/admin/user-list";
     }
 
     // 유저 전체 조회
