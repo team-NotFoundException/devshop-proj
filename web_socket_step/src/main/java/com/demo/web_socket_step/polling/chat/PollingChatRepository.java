812ffdcfb0087b567f775e7425d319a924267813
kim-se-hooh <wwb8524@naver.com>
2026-01-12 17:39:31 +0900

    save

--- web_socket_step/src/main/java/com/demo/web_socket_step/polling/chat/PollingChatRepository.java
+++ web_socket_step/src/main/java/com/demo/web_socket_step/polling/chat/PollingChatRepository.java
@@ -1,4 +1,6 @@
 package com.demo.web_socket_step.polling.chat;
 
-public interface PollingChatRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PollingChatRepository extends JpaRepository<Chat, Long> {
 }
