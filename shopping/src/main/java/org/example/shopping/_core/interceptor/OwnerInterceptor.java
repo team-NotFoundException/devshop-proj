3fade26ae74b9c4aca2766bcf9e807d53d76c323
Kitbomin <zzxxcc1433@naver.com>
2026-01-14 15:21:53 +0900

    suspension Owner complete

--- shopping/src/main/java/org/example/shopping/_core/interceptor/OwnerInterceptor.java
+++ shopping/src/main/java/org/example/shopping/_core/interceptor/OwnerInterceptor.java
@@ -3,14 +3,22 @@ package org.example.shopping._core.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.example.shopping._core.errors.exception.Exception401;
 import org.example.shopping._core.errors.exception.Exception403;
 import org.example.shopping.users.User;
+import org.example.shopping.users.enums.OwnerStatus;
+import org.example.shopping.users.owner.Owner;
+import org.example.shopping.users.owner.OwnerRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
+@RequiredArgsConstructor
 public class OwnerInterceptor implements HandlerInterceptor {
+    private final OwnerRepository ownerRepository;
+
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
@@ -24,6 +32,13 @@ public class OwnerInterceptor implements HandlerInterceptor {
 
         if (!sessionUser.getIsOwner()) {
             throw new Exception403("입점자 권한이 없어요");
+        }
+
+        Owner owner = ownerRepository.findByUserIdUseInterceptor(sessionUser.getId())
+                .orElseThrow(() -> new Exception403("입점사 정보를 찾을 수 없습니다."));
+
+        if (owner.getStatus() == OwnerStatus.SUSPENSION) {
+            throw new Exception403("관리자에 의해 활동이 정지된 입점사입니다.");
         }
 
         return true;
--- shopping/src/main/java/org/example/shopping/users/owner/OwnerRepository.java
+++ shopping/src/main/java/org/example/shopping/users/owner/OwnerRepository.java
@@ -1,6 +1,8 @@
 package org.example.shopping.users.owner;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +10,7 @@ import java.util.Optional;
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
     Optional<Owner> findByUserId(Long userId);
+
+    @Query("select o from  Owner o where o.user.id = :userId")
+    Optional<Owner> findByUserIdUseInterceptor(@Param("userId") Long userId);
 }
